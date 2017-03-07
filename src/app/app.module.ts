77edc59cf06e3c38abb3c145d28b7a079f71a63c
Jenniffer Cubillos <jen.cubillos@gmail.com>
2017-03-07 19:01:49 +0100

    Step: Master Detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
+//Defines AppModule, the root module that tells Angular how to assemble the application.
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule }   from '@angular/forms';
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+//Compiles the application with the JIT compiler and bootstraps the application's main module (AppModule) to run in the browser.
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
